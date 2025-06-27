af08be2bc953cb30d2b99d99c7863715bb2d12b8
hiruna534 <hiruna@unsw.edu.au>
2025-06-27 12:30:33 +1000

    update nci_gadi script

--- scripts/nci_gadi.sh
+++ scripts/nci_gadi.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-#PBS -P ox63
-#PBS -N svclassifier_1
+#PBS -P project
+#PBS -N test
 #PBS -l storage=gdata/kr68+gdata/ox63+gdata/if89
 #PBS -l ncpus=48
 #PBS -l mem=128GB
 #PBS -l walltime=20:00:00
 #PBS -l wd
-#PBS -V
+
 
 die() { echo -e "$1" >&2 ; echo ; exit 1 ; } # terminate script
-info() {  echo -e "$1" >&2 ; }
-info "$(date)"
 
-export MODULEPATH=/g/data/ox63/install/modules:$MODULEPATH
+#export MODULEPATH=/g/data/ox63/install/modules:$MODULEPATH
+module use /g/data/ox63/install/modules || die "could not use module path"
 module load svclass/1.0 || die "could not load svclass module"
 
-echo "--output_dir $OUTPUT_DIR --sample $SAMPLE --sv_vcf $SV_VCF --ref_fasta $REF"
-svclass --output_dir $OUTPUT_DIR --sample $SAMPLE --sv_vcf $SV_VCF --ref_fasta $REF || die "could not run svclass"
+echo "--out $OUTPUT_DIR --vcf $VCF --ref $REF"
+svclass --out $OUTPUT_DIR --vcf $VCF --ref $REF || die "could not run svclass"
